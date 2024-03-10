2c17c28294919565c5687b5582b8ebd3a9c09527
Don Sawdai <dsawdai@ieee.org>
2024-03-10 13:27:24 -0700

    Fixed conditional command for piece trigger

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -12,6 +12,7 @@ import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.ConditionalCommand;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
@@ -130,7 +131,7 @@ public class RobotContainer {
     intakeStopTrigger.onTrue(
       new ConditionalCommand(
         new StopIntakingSequence(feeder, intake, robotState, log),
-        null, 
+        new WaitCommand(0.01), 
         () -> robotState.getState() == State.INTAKE_TO_FEEDER)      
       );
   }
