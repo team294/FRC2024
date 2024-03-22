2222281d0dcf03e637c511de276165ab6476e490
BCR <frc294@gmail.com>
2024-03-21 18:32:03 -0700

    Set shooter percent to negative to prevent shooter from spitting out piece

--- src/main/java/frc/robot/commands/Autos/CenterTwoPieceShoot.java
+++ src/main/java/frc/robot/commands/Autos/CenterTwoPieceShoot.java
@@ -15,6 +15,7 @@ import frc.robot.Constants.StopType;
 import frc.robot.Constants.WristConstants.WristAngle;
 import frc.robot.commands.DriveResetPose;
 import frc.robot.commands.DriveTrajectory;
+import frc.robot.commands.ShooterSetPercent;
 import frc.robot.commands.WristSetAngle;
 import frc.robot.commands.Sequences.IntakePieceAuto;
 import frc.robot.commands.Sequences.SetShooterWristSpeaker;
@@ -42,6 +43,7 @@ public class CenterTwoPieceShoot extends SequentialCommandGroup {
       new SetShooterWristSpeaker(WristAngle.speakerShotFromSpeaker, 
         ShooterConstants.shooterVelocityTop, ShooterConstants.shooterVelocityBottom, shooter, wrist, intake, feeder, robotState, log),
       new ShootPiece(ShooterConstants.shooterVelocityTop, ShooterConstants.shooterVelocityBottom, shooter, feeder, robotState, log),
+      new ShooterSetPercent(-0.02, shooter, log),
       new ParallelCommandGroup(
         new IntakePieceAuto(intake, feeder, robotState, log),
         new WristSetAngle(WristAngle.lowerLimit, wrist, log),
