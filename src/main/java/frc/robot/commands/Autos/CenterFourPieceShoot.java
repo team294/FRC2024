b73e0acb3bb8978b3b4bc6088665a172fec8ee8f
BCR <frc294@gmail.com>
2024-03-22 10:59:53 -0700

    Change start of four piece auto

--- src/main/java/frc/robot/commands/Autos/CenterFourPieceShoot.java
+++ src/main/java/frc/robot/commands/Autos/CenterFourPieceShoot.java
@@ -9,14 +9,19 @@ import edu.wpi.first.wpilibj2.command.ConditionalCommand;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.Constants.CoordType;
+import frc.robot.Constants.ShooterConstants;
 import frc.robot.Constants.StopType;
+import frc.robot.Constants.WristConstants.*;
 import frc.robot.commands.DriveTrajectory;
+import frc.robot.commands.*;
 import frc.robot.commands.Sequences.IntakePieceAuto;
+import frc.robot.commands.Sequences.SetShooterWristSpeaker;
 import frc.robot.commands.Sequences.ShootPiece;
 import frc.robot.subsystems.DriveTrain;
 import frc.robot.subsystems.Feeder;
 import frc.robot.subsystems.Intake;
 import frc.robot.subsystems.Shooter;
+import frc.robot.subsystems.Wrist;
 import frc.robot.utilities.AllianceSelection;
 import frc.robot.utilities.BCRRobotState;
 import frc.robot.utilities.FileLog;
@@ -28,26 +33,43 @@ import frc.robot.utilities.TrajectoryCache.TrajectoryType;
 // https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
 public class CenterFourPieceShoot extends SequentialCommandGroup {
   /** Creates a new CenterFourPieceShoot. */
-  public CenterFourPieceShoot(Intake intake, Shooter shooter, DriveTrain driveTrain, Feeder feeder, BCRRobotState robotState, TrajectoryCache cache, AllianceSelection alliance, FileLog log) {
+  public CenterFourPieceShoot(Intake intake, Wrist wrist, Shooter shooter, DriveTrain driveTrain, Feeder feeder, BCRRobotState robotState, TrajectoryCache cache, AllianceSelection alliance, FileLog log) {
     // Add your commands in the addCommands() call, e.g.
     // addCommands(new FooCommand(), new BarCommand());
     addCommands(
-      new ShootPiece(shooter, feeder, robotState, log),
+      new SetShooterWristSpeaker(WristAngle.speakerShotFromSpeaker, 
+        ShooterConstants.shooterVelocityTop, ShooterConstants.shooterVelocityBottom, shooter, wrist, intake, feeder, robotState, log),
+      new ShootPiece(ShooterConstants.shooterVelocityTop, ShooterConstants.shooterVelocityBottom, shooter, feeder, robotState, log),
+      new ShooterSetPercent(-0.02, shooter, log),
       new ParallelCommandGroup(
+        new WristSetAngle(WristAngle.lowerLimit, wrist, log),
         new IntakePieceAuto(intake, feeder, robotState, log),
         new ConditionalCommand(
-          new DriveTrajectory(CoordType.kAbsolute, StopType.kBrake, cache.cache[TrajectoryType.driveCenterAmpNoteRed.value], driveTrain, log), 
-          new DriveTrajectory(CoordType.kAbsolute, StopType.kBrake, cache.cache[TrajectoryType.driveCenterAmpNoteBlue.value], driveTrain, log), 
+          new SequentialCommandGroup(
+            new DriveResetPose(1.3, 2.663, 0, false, driveTrain, log),
+            new DriveTrajectory(CoordType.kAbsolute, StopType.kBrake, cache.cache[TrajectoryType.driveCenterAmpNoteRed.value], driveTrain, log)
+          )
+          , 
+          new SequentialCommandGroup(
+            new DriveResetPose(1.3, 5.57, 0, false, driveTrain, log),
+            new DriveTrajectory(CoordType.kAbsolute, StopType.kBrake, cache.cache[TrajectoryType.driveCenterAmpNoteBlue.value], driveTrain, log)
+          ), 
           () -> alliance.getAlliance() == Alliance.Red
         )
       ),
-      new ConditionalCommand(
+      new ParallelCommandGroup(
+        new ConditionalCommand(
           new DriveTrajectory(CoordType.kAbsolute, StopType.kBrake, cache.cache[TrajectoryType.driveFromAmpNoteToCenterStartRed.value], driveTrain, log), 
           new DriveTrajectory(CoordType.kAbsolute, StopType.kBrake, cache.cache[TrajectoryType.driveFromAmpNoteToCenterStartBlue.value], driveTrain, log), 
           () -> alliance.getAlliance() == Alliance.Red
+        ),
+        new SetShooterWristSpeaker(WristAngle.speakerShotFromSpeaker, 
+          ShooterConstants.shooterVelocityTop, ShooterConstants.shooterVelocityBottom, shooter, wrist, intake, feeder, robotState, log)
       ),
       new ShootPiece(shooter, feeder, robotState, log),
+      new ShooterSetPercent(-0.02, shooter, log),
       new ParallelCommandGroup(
+        new WristSetAngle(WristAngle.lowerLimit, wrist, log),
         new IntakePieceAuto(intake, feeder, robotState, log),
         new ConditionalCommand(
           new DriveTrajectory(CoordType.kAbsolute, StopType.kBrake, cache.cache[TrajectoryType.driveToCenterCloseNoteRed.value], driveTrain, log), 
@@ -55,26 +77,18 @@ public class CenterFourPieceShoot extends SequentialCommandGroup {
           () -> alliance.getAlliance() == Alliance.Red
         )
       ),
-      new ConditionalCommand(
+      new ParallelCommandGroup(
+        new ConditionalCommand(
           new DriveTrajectory(CoordType.kAbsolute, StopType.kBrake, cache.cache[TrajectoryType.driveFromCenterNoteToCenterStartRed.value], driveTrain, log), 
           new DriveTrajectory(CoordType.kAbsolute, StopType.kBrake, cache.cache[TrajectoryType.driveFromCenterNoteToCenterStartBlue.value], driveTrain, log), 
           () -> alliance.getAlliance() == Alliance.Red
+        ),
+        new SetShooterWristSpeaker(WristAngle.speakerShotFromSpeaker, 
+          ShooterConstants.shooterVelocityTop, ShooterConstants.shooterVelocityBottom, shooter, wrist, intake, feeder, robotState, log)
       ),
       new ShootPiece(shooter, feeder, robotState, log),
-      new ParallelCommandGroup(
-        new IntakePieceAuto(intake, feeder, robotState, log),
-        new ConditionalCommand(
-          new DriveTrajectory(CoordType.kAbsolute, StopType.kBrake, cache.cache[TrajectoryType.driveCenterToNearSourceRed.value], driveTrain, log), 
-          new DriveTrajectory(CoordType.kAbsolute, StopType.kBrake, cache.cache[TrajectoryType.driveCenterToNearSourceBlue.value], driveTrain, log), 
-          () -> alliance.getAlliance() == Alliance.Red
-        )
-      ),
-      new ConditionalCommand(
-          new DriveTrajectory(CoordType.kAbsolute, StopType.kBrake, cache.cache[TrajectoryType.driveFromSourceNearToCenterStartRed.value], driveTrain, log), 
-          new DriveTrajectory(CoordType.kAbsolute, StopType.kBrake, cache.cache[TrajectoryType.driveFromSourceNearToCenterStartBlue.value], driveTrain, log), 
-          () -> alliance.getAlliance() == Alliance.Red
-      ),
-      new ShootPiece(shooter, feeder, robotState, log)
+      new ShooterSetPercent(-0.02, shooter, log),
+      new WristSetAngle(WristAngle.lowerLimit, wrist, log)
     );
   }
 }
