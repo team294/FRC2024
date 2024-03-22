1250977a2a0916e12bc50a2c0b022562466a8761
BCR <146675200+ArtemZhukovskyy@users.noreply.github.com>
2024-03-22 16:13:33 -0700

    Goes to center at the end of 3 piece shoot

--- src/main/java/frc/robot/utilities/TrajectoryCache.java
+++ src/main/java/frc/robot/utilities/TrajectoryCache.java
@@ -312,18 +312,18 @@ public class TrajectoryCache {
 
         cache[TrajectoryType.driveFromCenterStartToEndCenterAutoRed.value] = new TrajectoryFacing(new Rotation2d(0), 
             new Rotation2d(0), 
-            calcTrajectory("Center Start to near note Red", .4, .4, false, 
-            new Pose2d(1.3, 2.663, new Rotation2d(-17)), 
+            calcTrajectory("Center Start to near note Red", .8, .4, false, 
+            new Pose2d(1.3, 2.663, new Rotation2d(Math.toRadians(-17))), 
             List.of(), 
-            new Pose2d(7, 1.7, new Rotation2d(-17))
+            new Pose2d(7, 1.7, new Rotation2d(Math.toRadians(-17)))
             ));
 
         cache[TrajectoryType.driveFromCenterStartToEndCenterAutoBlue.value] = new TrajectoryFacing(new Rotation2d(0), 
             new Rotation2d(0), 
-            calcTrajectory("Center Start to near note Blue", .4, .4, false, 
-            new Pose2d(1.3, 5.57, new Rotation2d(17)), 
+            calcTrajectory("Center Start to near note Blue", .8, .4, false, 
+            new Pose2d(1.3, 5.57, new Rotation2d(Math.toRadians(17))), 
             List.of(), 
-            new Pose2d(7, 6.5, new Rotation2d(17))
+            new Pose2d(7, 6.5, new Rotation2d(Math.toRadians(17)))
             ));
 
         
