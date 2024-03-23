a515ae6d64dd0f45b16255110fed48f28c90ac9d
Don Sawdai <dsawdai@ieee.org>
2024-03-23 10:40:37 -0700

    Removed unused import

--- src/main/java/frc/robot/utilities/AutoSelection.java
+++ src/main/java/frc/robot/utilities/AutoSelection.java
@@ -20,7 +20,6 @@ import frc.robot.commands.Sequences.ShootPiece;
 import frc.robot.subsystems.DriveTrain;
 import frc.robot.subsystems.Feeder;
 import frc.robot.subsystems.Intake;
-import frc.robot.subsystems.LED;
 import frc.robot.subsystems.Shooter;
 import frc.robot.subsystems.Wrist;
 
