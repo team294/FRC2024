243502f4351b19388e5074511d28a228343d3b48
alexbloomer <alexbloomer@gmail.com>
2024-03-29 21:55:03 -0700

    Adjusted angles

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -352,9 +352,9 @@ public final class Constants {
           speakerShotFromPodium(-70),  // A4: changed to -70 deg.  Practice field -72deg for 128" field edge to front of bumper, ~144" to robot origin
           speakerShotFromMidStage(-79),
           farShotAngle(-83),
-          sourceCloseNoteShot(-60),
-          centerCloseNoteShot(-56),
-          ampCloseNoteShot(-62),
+          sourceCloseNoteShot(-65),
+          centerCloseNoteShot(-62),
+          ampCloseNoteShot(-67),
           overheadShotAngle(55),      // 135" field edge to front of bumper
           climbStop(-45.0),
           ampShot(50.0),
