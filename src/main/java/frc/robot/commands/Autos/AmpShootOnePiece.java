20c1e77c99637a690ef5ea915976c61e11f6d899
BCR <146675200+ArtemZhukovskyy@users.noreply.github.com>
2024-03-29 22:01:30 -0700

    Updated One Note autos

---
+++ src/main/java/frc/robot/commands/Autos/AmpShootOnePiece.java
@@ -0,0 +1,52 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands.Autos;
+
+import edu.wpi.first.wpilibj2.command.ConditionalCommand;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.Constants.CoordType;
+import frc.robot.Constants.ShooterConstants;
+import frc.robot.Constants.StopType;
+import frc.robot.Constants.WristConstants.WristAngle;
+import frc.robot.commands.DriveResetPose;
+import frc.robot.commands.DriveTrajectory;
+import frc.robot.commands.ShooterSetPercent;
+import frc.robot.commands.Sequences.SetShooterWristSpeaker;
+import frc.robot.commands.Sequences.ShootPiece;
+import frc.robot.subsystems.DriveTrain;
+import frc.robot.subsystems.Feeder;
+import frc.robot.subsystems.Intake;
+import frc.robot.subsystems.Shooter;
+import frc.robot.subsystems.Wrist;
+import frc.robot.utilities.AllianceSelection;
+import frc.robot.utilities.BCRRobotState;
+import frc.robot.utilities.FileLog;
+import frc.robot.utilities.TrajectoryCache;
+import frc.robot.utilities.TrajectoryCache.TrajectoryType;
+
+// NOTE:  Consider using this command inline, rather than writing a subclass.  For more
+// information, see:
+// https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
+public class AmpShootOnePiece extends SequentialCommandGroup {
+  /** Creates a new AmpShootOnePIece. */
+  public AmpShootOnePiece(Intake intake, Wrist wrist, Shooter shooter, DriveTrain driveTrain, Feeder feeder, BCRRobotState robotState, TrajectoryCache cache, AllianceSelection alliance, FileLog log) {
+    // Add your commands in the addCommands() call, e.g.
+    // addCommands(new FooCommand(), new BarCommand());
+    addCommands(
+      // new ConditionalCommand(
+      //   new DriveResetPose(0.2, 1.2, -60, true, driveTrain, log),
+      //   new DriveResetPose(0.2, 7.0, 60, true, driveTrain, log),
+      //   () -> alliance.getAlliance() == Alliance.Red
+      // ),
+      // new SetShooterWristSpeaker(WristAngle.speakerShotFromSpeaker, ShooterConstants.shooterVelocityTop, ShooterConstants.shooterVelocityBottom, shooter, wrist, intake, feeder, robotState, log),
+      // new ShootPiece(ShooterConstants.shooterVelocityTop, ShooterConstants.shooterVelocityBottom, shooter, feeder, robotState, log),
+      // new ShooterSetPercent(-0.02, shooter, log)
+      new SetShooterWristSpeaker(WristAngle.speakerShotFromSpeaker, 
+        ShooterConstants.shooterVelocityTop, ShooterConstants.shooterVelocityBottom, shooter, wrist, intake, feeder, robotState, log),
+      new ShootPiece(ShooterConstants.shooterVelocityTop, ShooterConstants.shooterVelocityBottom, shooter, feeder, robotState, log),
+      new ShooterSetPercent(-0.02, shooter, log)
+    );
+  }
+}
