76041bf30678550c7c98efe6a3045174ddfc434c
BCR294-Laptop19 <cameron.s.murray57@gmail.com>
2024-03-29 20:48:41 -0700

    Increased Climb Speed Further

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -305,9 +305,9 @@ public final class Constants {
       
       public static final double voltageCompSaturation = 12.0;
       public static final double maxUncalibratedPercentOutput = 0.15;     // CALIBRATED
-      public static final double maxPercentOutput = 0.4;          // CALIBRATED
+      public static final double maxPercentOutput = 0.5;          // CALIBRATED
 
-      public static final double climbPercentOutput = -0.4;
+      public static final double climbPercentOutput = -0.5;
 
       // Update the REV through bore encoder offset angle in RobotPreferences (in Shuffleboard), not in this code!
       // After updating in RobotPreferences, you will need to re-start the robot code for the changes to take effect.
