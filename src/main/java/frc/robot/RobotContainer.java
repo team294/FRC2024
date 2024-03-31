1730fb60d5c497637d58a31e82ba83f46b611cf6
Don Sawdai <dsawdai@ieee.org>
2024-03-31 15:00:28 -0700

    B1:  Adjustments with robot design update

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -43,7 +43,7 @@ import frc.robot.utilities.BCRRobotState.State;
  */
 public class RobotContainer {
   // Define robot key utilities (DO THIS FIRST)
-  private final FileLog log = new FileLog("A8");
+  private final FileLog log = new FileLog("B1");
   private final AllianceSelection allianceSelection = new AllianceSelection(log);
 
   // Define robot subsystems  
