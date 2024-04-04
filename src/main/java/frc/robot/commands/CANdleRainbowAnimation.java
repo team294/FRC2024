af77cf91130c5e9146ff6cb618074759027fd1c0
BCR <146675200+ArtemZhukovskyy@users.noreply.github.com>
2024-04-04 15:24:08 -0700

    no blinks during rainbow anim

--- src/main/java/frc/robot/commands/CANdleRainbowAnimation.java
+++ src/main/java/frc/robot/commands/CANdleRainbowAnimation.java
@@ -33,8 +33,9 @@ public class CANdleRainbowAnimation extends Command {
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-    RainbowAnimation anim = new RainbowAnimation(1, .4, segment.count, false, segment.index);
+    RainbowAnimation anim = new RainbowAnimation(1, .7, segment.count, false, segment.index);
     led.animate(anim);
+    led.setRainbow();
   }
 
   // Called every time the scheduler runs while the command is scheduled.
