12a7cdcce22740a6127a38e14dfbc0c4a49b84a4
limrungroj <the.great.schweb@gmail.com>
2024-04-17 10:34:53 -0700

    removed un-needed imports

--- src/main/java/frc/robot/commands/Autos/SourceFourNoteCenter.java
+++ src/main/java/frc/robot/commands/Autos/SourceFourNoteCenter.java
@@ -4,9 +4,6 @@
 
 package frc.robot.commands.Autos;
 
-import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.proto.Trajectory;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj2.command.ConditionalCommand;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
