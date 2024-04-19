be4234900d23733e62c3a93ab4487752e4dc3a14
limrungroj <the.great.schweb@gmail.com>
2024-04-19 07:08:00 -0500

    changed angle

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -411,8 +411,8 @@ public final class Constants {
           ampCloseNoteShot(-63),
           endFiveNoteShot(-72),
           sourceThreePieceShot(-69),
-          endAmpFourcePieceShot(-68), // last note shot for amp 4 note 
-          ampFourPieceShot(-75),     // C6:  Decreased by 3 degrees.  normal shot for amp 4 note
+          endAmpFourcePieceShot(-66), // last note shot for amp 4 note 
+          ampFourPieceShot(-73),     // C6:  Decreased by 3 degrees.  normal shot for amp 4 note
           overheadShotAngle(56),      // B5:  Increased from 55 to 56 deg after Qual 63
           climbStop(-45.0),
           ampShot(52.0),            // C4:  Increased from 50 to 52
