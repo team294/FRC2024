3c9ea00fc425bd0793aca81e082eb88845f35ac2
BCR <146675203+antlikesbees@users.noreply.github.com>
2024-04-20 08:01:21 -0700

    Added the drive to pose to go back slightly

--- src/main/java/frc/robot/commands/Autos/SourceOnePieceDriveToFifthNote.java
+++ src/main/java/frc/robot/commands/Autos/SourceOnePieceDriveToFifthNote.java
@@ -4,15 +4,20 @@
 
 package frc.robot.commands.Autos;
 
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj2.command.ConditionalCommand;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import edu.wpi.first.wpilibj2.command.ParallelDeadlineGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitUntilCommand;
 import frc.robot.Constants.CoordType;
 import frc.robot.Constants.ShooterConstants;
 import frc.robot.Constants.StopType;
 import frc.robot.Constants.WristConstants.WristAngle;
 import frc.robot.commands.DriveResetPose;
+import frc.robot.commands.DriveToPose;
 import frc.robot.commands.DriveTrajectory;
 import frc.robot.commands.WristSetAngle;
 import frc.robot.commands.Sequences.IntakePieceAuto;
@@ -32,15 +37,15 @@ import frc.robot.utilities.TrajectoryCache.TrajectoryType;
 // NOTE:  Consider using this command inline, rather than writing a subclass.  For more
 // information, see:
 // https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
-public class SourceOnePieceMobilityAuto extends SequentialCommandGroup {
+public class SourceOnePieceDriveToFifthNote extends SequentialCommandGroup {
   /** Creates a new SourceOnePieceAuto. */
-  public SourceOnePieceMobilityAuto(Intake intake, Wrist wrist, Shooter shooter, DriveTrain driveTrain, Feeder feeder, BCRRobotState robotState, TrajectoryCache cache, AllianceSelection alliance, FileLog log) {
+  public SourceOnePieceDriveToFifthNote(Intake intake, Wrist wrist, Shooter shooter, DriveTrain driveTrain, Feeder feeder, BCRRobotState robotState, TrajectoryCache cache, AllianceSelection alliance, FileLog log) {
     // Add your commands in the addCommands() call, e.g.
     // addCommands(new FooCommand(), new BarCommand());
     addCommands(
       new SetShooterWristSpeakerAuto(WristAngle.speakerShotFromSpeaker, ShooterConstants.shooterVelocityTop, ShooterConstants.shooterVelocityBottom, shooter, wrist, intake, feeder, robotState, log),
       new ShootPiece(ShooterConstants.shooterVelocityTop, ShooterConstants.shooterVelocityBottom, true, shooter, feeder, wrist, robotState, log),
-      new ParallelCommandGroup(
+      new ParallelDeadlineGroup(
         new ConditionalCommand(
           new SequentialCommandGroup(
               new DriveResetPose(0.8, 3.7296, 60, false, driveTrain, log),
@@ -51,9 +56,18 @@ public class SourceOnePieceMobilityAuto extends SequentialCommandGroup {
               new DriveTrajectory(CoordType.kAbsolute, StopType.kBrake, cache.cache[TrajectoryType.driveFromSourceToSideMobilityBlue.value], driveTrain, log) 
           ),
           () -> alliance.getAlliance() == Alliance.Red
-        ),
+        ).andThen( new WaitUntilCommand( () -> feeder.getFeederSetPercent() == 0.0 ).withTimeout(0.5) ),
         new WristSetAngle(WristAngle.lowerLimit, wrist, log),
         new IntakePieceAuto(intake, feeder, robotState, log)
+      ),
+      new ConditionalCommand(
+          new SequentialCommandGroup(
+            new DriveToPose(new Pose2d(7.0, 7.467, new Rotation2d(0)), driveTrain, log)
+          ),
+          new SequentialCommandGroup(
+            new DriveToPose(new Pose2d(7.0, 0.7696, new Rotation2d(0)), driveTrain, log)
+          ),
+          () -> alliance.getAlliance() == Alliance.Red
       )
     );
   }
