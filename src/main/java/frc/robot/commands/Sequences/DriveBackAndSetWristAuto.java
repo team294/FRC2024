98d44efcf19c9b5a12c35b7709e9046ea9f2ba05
Eric EH <superchavacool@gmail.com>
2024-09-08 14:36:54 -0700

    Changed DriveBackAndSetWristAuto to be a parallel command group rather than deadline

--- src/main/java/frc/robot/commands/Sequences/DriveBackAndSetWristAuto.java
+++ src/main/java/frc/robot/commands/Sequences/DriveBackAndSetWristAuto.java
@@ -4,7 +4,7 @@
 
 package frc.robot.commands.Sequences;
 
-import edu.wpi.first.wpilibj2.command.ParallelDeadlineGroup;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import frc.robot.Constants.CoordType;
 import frc.robot.Constants.ShooterConstants;
 import frc.robot.Constants.StopType;
@@ -24,7 +24,7 @@ import frc.robot.utilities.AllianceSelection;
 // NOTE:  Consider using this command inline, rather than writing a subclass.  For more
 // information, see:
 // https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
-public class DriveBackAndSetWristAuto extends ParallelDeadlineGroup {
+public class DriveBackAndSetWristAuto extends ParallelCommandGroup {
   /** 
    * Drives to a given trajectory based on alliance, and primes the Shooter and Wrist for a speaker shot.
    * @param trajectory trajectory to follow (Changing with alliance)
@@ -40,10 +40,10 @@ public class DriveBackAndSetWristAuto extends ParallelDeadlineGroup {
    * @param log
   */
   public DriveBackAndSetWristAuto(TrajectoryType trajectory, WristAngle wristAngle, DriveTrain drivetrain, Feeder feeder, Shooter shooter, Wrist wrist, Intake intake, BCRRobotState robotState, TrajectoryCache cache, AllianceSelection alliance, FileLog log) {
-    // Add the deadline command in the super() call. 
-    // Add other commands using addCommands().
-    super(new DriveTrajectory(CoordType.kAbsolute, StopType.kBrake, cache.cache[trajectory.value], drivetrain, alliance, log));
+    // Add your commands in the addCommands() call, e.g.
+    // addCommands(new FooCommand(), new BarCommand());
     addCommands(
+      new DriveTrajectory(CoordType.kAbsolute, StopType.kBrake, cache.cache[trajectory.value], drivetrain, alliance, log),
       new SetShooterWristSpeakerAuto(wristAngle, ShooterConstants.shooterVelocityTop, ShooterConstants.shooterVelocityBottom, shooter, wrist, intake, feeder, robotState, log)
     );
   }
